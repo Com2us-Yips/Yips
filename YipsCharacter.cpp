e3c5bb6542419828f34cc1d57989ab32735a76c8
LeeTaeWook <70584570+xodnr7727@users.noreply.github.com>
2024-11-14 13:15:21 +0900

    Update YipsCharacter.cpp

--- YipsCharacter.cpp
+++ YipsCharacter.cpp
@@ -1,22 +1,135 @@
 
 
 #include "YipsCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 #include "Containers/Array.h"
 #include "Math/UnrealMathUtility.h"
 
 AYipsCharacter::AYipsCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	// Set size for collision capsule
+GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	
+// Don't rotate when the controller rotates. Let that just affect the camera.
+bUseControllerRotationPitch = false;
+bUseControllerRotationYaw = false;
+bUseControllerRotationRoll = false;
 
-    ChapterInt = 1;
-	MaxActionsPerDayInt = 3; // 첫 챕터에서 하루 행동량
-	CurrentActionsInt = 0;
-	StrikesInt = 0;
-	BallsInt = 0;
-	CurrentOrderIndexInt = 0;
+// Configure character movement
+GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
 
-    InitializeNewChapter();
+// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
+// instead of recompiling to adjust them
+GetCharacterMovement()->JumpZVelocity = 700.f;
+GetCharacterMovement()->AirControl = 0.35f;
+GetCharacterMovement()->MaxWalkSpeed = 500.f;
+GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+
+// Create a camera boom (pulls in towards the player if there is a collision)
+CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+CameraBoom->SetupAttachment(RootComponent);
+CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+// Create a follow camera
+FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+	
+ChapterInt = 1;
+MaxActionsPerDayInt = 3; // 첫 챕터에서 하루 행동량
+CurrentActionsInt = 0;
+StrikesInt = 0;
+BallsInt = 0;
+CurrentOrderIndexInt = 0;
+
+InitializeNewChapter();
+}
+
+void AYipsCharacter::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Input
+
+void AYipsCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+		
+		//Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+		//Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AYipsCharacter::Move);
+
+		//Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AYipsCharacter::Look);
+
+	}
+
+}
+
+void AYipsCharacter::Move(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement 
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AYipsCharacter::Look(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
 
 void AYipsCharacter::InitializeNewChapter()
@@ -109,21 +222,4 @@ void AYipsCharacter::NextChapter()
     InitializeNewChapter(); // 챕터 전환 후 하루 행동 초기화
 }
 
-void AYipsCharacter::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-void AYipsCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-void AYipsCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
